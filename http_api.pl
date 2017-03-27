06bde41ba955c660b8eb46b930ebd087373bdf88
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2017-03-27 16:50:00 -0400

    Added working HTTP/JSON API

---
+++ http_api.pl
@@ -0,0 +1,26 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_error)).
+
+:- [defuser].
+
+server(Port) :- http_server(http_dispatch, [port(Port)]).
+
+:- http_handler('/api', handle, []).
+
+read_term_from_atom([],[]). % need special case for empty list
+read_term_from_atom(A,T) :- read_term_from_atom(A,T,[]).
+
+handle(Request) :-
+    http_read_json(Request, JSONIn),
+    json_to_prolog(JSONIn, [Pred|Args]),
+    maplist(read_term_from_atom, Args, ArgsTerms),
+    member(Pred, [numSolutions, defused]), % add all relevant predicates
+    aggregate_all(set([Pred|ArgsTerms]), (Goal =.. [Pred|ArgsTerms], call(Goal)), Results),
+    maplist(maplist(term_to_atom), Results, Output),
+    prolog_to_json(Output, JSONOut),
+    reply_json(JSONOut).
+
